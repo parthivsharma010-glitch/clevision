b5f770ed6bd3772622928b878fb004b1fc4f7f4b
pansara purvin <parthivsharma010@gmail.com>
2026-06-16 13:34:14 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+function Header(){
+    return(    <>
+    <div className="head">
+        <img src="https://th.bing.com/th/id/OIP.1Tspqo5APgk0Knq-ThBzFwHaHa?w=195&h=195&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="" />
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li> contac</li>
+        </ul>
+        
+        </div>
+        </>)
+}
+
+export default Header;
